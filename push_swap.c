0ab94fd036f784bbb225ed0a810e850393109a4e
smelicha <smelicha@student.42heilbronn.de>
2023-10-12 18:08:21 +0200

    now it seems I create list and delete it successfully without leaks, so far

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: smelicha <smelicha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/08 18:42:03 by smelicha          #+#    #+#             */
-/*   Updated: 2023/10/12 17:56:41 by smelicha         ###   ########.fr       */
+/*   Updated: 2023/10/12 18:07:47 by smelicha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,19 @@
 
 //void	check_leaks();
 
+void	data_init(t_dt *dt)
+{
+	dt->a = ft_lstnew(NULL);
+	dt->b = ft_lstnew(NULL);
+	dt->head_a = dt->a;
+	dt->head_b = dt->b;
+}
+void	data_free(t_dt *dt)
+{
+	ft_lstclear(&dt->head_a, NULL);
+	ft_lstclear(&dt->head_b, NULL);
+	free(dt);
+}
 
 int	main(int argc, const char *argv[])
 {
@@ -21,10 +34,12 @@ int	main(int argc, const char *argv[])
 
 	dt = NULL;
 	printf("%i\n", argc);
+	ft_putstr_fd((char *)argv[0], 1);
 	dt = malloc(sizeof(dt));
 	if (!dt)
 		return (-1);
-	ft_putstr_fd((char *)argv[0], 1);
+	data_init(dt);
 //	check_leaks();
+//	system("leaks push_swap");
 	return (0);
 }
