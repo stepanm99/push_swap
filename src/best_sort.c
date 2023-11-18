877f2c5aeb605ed4098305caf2b9c400a2f75383
stepanm99 <stepan.melichar99@seznam.cz>
2023-11-18 23:15:11 +0100

    added calculate_b_rot_cost function which should return the cost of rotating stack b with simultaneous rotation in mind

--- src/best_sort.c
+++ src/best_sort.c
@@ -6,7 +6,7 @@
 /*   By: stepan <stepan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/11 17:14:26 by stepan            #+#    #+#             */
-/*   Updated: 2023/11/18 22:32:32 by stepan           ###   ########.fr       */
+/*   Updated: 2023/11/18 23:14:17 by stepan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,35 @@ void	b_rotation(t_dt *dt)
 {
 
 }*/
+
+/*need to work out the logic :(*/
+int	calculate_b_rot_cost(int a_position, t_dt *dt)
+{
+	int	cost;
+	int	a_stack_middle;
+	int	rev_flag;
+
+	a_stack_middle = dt->a_length / 2;
+	if (a_position <= a_stack_middle)
+		rev_flag = 0;
+	else
+		rev_flag = 1;
+	if (dt->sort_data.b_rev_rot < 0)
+	{
+		if (a_position < dt->sort_data.b_rot)
+
+		else
+
+	}
+	if (dt->sort_data.b_rot < 0)
+	{
+		if (a_positon < dt->sort_data.b_rev_rot)
+
+		else
+
+	}
+	return (cost);
+}
 
 /*How many move would it take to put this number into correct position*/
 void	calculate_cost(t_dt *dt)
