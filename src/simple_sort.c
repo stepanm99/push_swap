2b02d75372b8aa68406f5a5614b5ad4cec480a2d
stepanm99 <stepan.melichar99@seznam.cz>
2023-11-24 23:00:06 +0100

    simple_sort and better_sort are out of sort.c, everything compiles, everything works. now I will fix some too long functions

--- src/simple_sort.c
+++ src/simple_sort.c
@@ -6,8 +6,186 @@
 /*   By: smelicha <smelicha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/24 22:47:19 by smelicha          #+#    #+#             */
-/*   Updated: 2023/11/24 22:47:34 by smelicha         ###   ########.fr       */
+/*   Updated: 2023/11/24 22:58:26 by smelicha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../incl/push_swap.h"
+
+void	min_index(t_dt *dt,	t_link *head, int range, char max_flag)
+{
+	t_link			*temp;
+	unsigned int	limit;
+	unsigned int	min_index;
+	char			back_flag;
+
+	if (!head)
+		error(dt);
+	back_flag = 0;
+	limit = range;
+	if (head == dt->head_a)
+	{
+		if (range == 0)
+			limit = dt->a_length;
+		else
+		{
+			limit = range;
+			back_flag = 1;
+		}
+		temp = dt->head_a->next;
+	}
+	else if (head == dt->head_b)
+	{
+		if (range == 0)
+			limit = dt->b_length;
+		else
+		{
+			limit = range;
+			back_flag = 1;
+		}
+		temp = dt->head_b->next;
+	}
+	if (!max_flag)
+		min_index = 4294967295;
+	else
+		min_index = 0;
+	while (limit && temp)
+	{
+		if (!max_flag)
+		{
+			if (temp->index < min_index && limit)
+				min_index = temp->index;
+		}
+		else
+		{
+			if (temp->index > min_index && limit)
+				min_index = temp->index;
+		}
+		temp = temp->next;
+		limit--;
+	}
+	dt->min_index = min_index;
+	if (back_flag)
+	{
+		limit = range;
+		temp = head->prev;
+		while (limit && temp != head)
+		{
+			if (temp->index < min_index && limit)
+				min_index = temp->index;
+			temp = temp->prev;
+			limit--;
+		}
+		dt->min_index = min_index;
+	}
+}
+
+unsigned int	find_min_index(t_dt *dt, t_link *head, int range)
+{
+	t_link			*temp;
+	unsigned int	limit;
+	unsigned int	i;
+
+	if (!head)
+		error(dt);
+	limit = range;
+	if (head == dt->head_a)
+	{
+		temp = dt->head_a->next;
+		if (range == 0)
+			limit = dt->a_length;
+		else
+			limit = range;
+	}
+	else if (head == dt->head_b)
+	{
+		temp = dt->head_b->next;
+		if (range == 0)
+			limit = dt->b_length;
+		else
+			limit = range;
+	}
+	i = 0;
+	while (limit)
+	{
+		if (temp->index == dt->min_index && limit)
+			return (i);
+		temp = temp->next;
+		i++;
+		limit--;
+	}
+	return (4294967295);
+}
+
+unsigned int	find_min_index_rev(t_dt *dt, t_link *head, int range)
+{
+	t_link			*temp;
+	unsigned int	limit;
+	unsigned int	i;
+
+	if (!head)
+		error(dt);
+	limit = range;
+	if(head == dt->head_a)
+	{
+		temp = dt->head_a->prev;
+		if (range == 0)
+			limit = dt->a_length;
+		else
+			limit = range;
+	}
+	else if (head == dt->head_b)
+	{
+		temp = dt->head_b->prev;
+		if (range == 0)
+			limit = dt->b_length;
+		else
+			limit = range;
+	}
+	i = 1;
+	while (limit)
+	{
+		if (temp->index == dt->min_index && limit)
+			return (i);
+		temp = temp->prev;
+		i++;
+		limit--;
+	}
+	return (4294967295);
+}
+
+void	simple_sort(t_dt *dt)
+{
+	unsigned int	forward_pos;
+	unsigned int	backward_pos;
+
+	forward_pos = 4294967295;
+	backward_pos = 4294967295;
+	while (dt->a_length)
+	{
+		min_index(dt, dt->head_a, 0, 0);
+		forward_pos = find_min_index(dt, dt->head_a, 0);
+		backward_pos = find_min_index_rev(dt, dt->head_a, 0);
+		if (forward_pos == backward_pos)
+			backward_pos = 4294967295;
+		if ((forward_pos < backward_pos) && (dt->head_a->next->index != dt->min_index))
+		{
+			while (forward_pos)
+			{
+				rotate_a(dt);
+				forward_pos--;
+			}
+		}
+		else if ((backward_pos < forward_pos) && (dt->head_a->next->index != dt->min_index))
+		{
+			while (backward_pos)
+			{
+				rev_rotate_a(dt);
+				backward_pos--;
+			}
+		}
+		push_b(dt);
+	}
+	while (dt->b_length)
+		push_a(dt);
+}
